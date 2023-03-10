8c1ffae57aeb36d611ab04c7ea3bc2053b86353e
Eri Alfonso <thecheese.gw2@gmail.com>
2023-03-10 12:12:20 -0300

    created item.js and itemcounter

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { useState } from 'react';
+
+
+const ItemCount = () => {
+// funciones
+const [counter, setCounter] = useState (0);
+const sumar = () => {
+    setCounter (counter + 1)
+}
+const restar = () =>{
+    setCounter (counter - 1)
+}
+return (
+    <>
+          <ButtonToolbar
+        className="justify-content-between"
+        aria-label="Toolbar with Button groups"
+      >
+    <ButtonGroup aria-label="Basic example">
+      <Button variant="outline-danger" onClick={restar}> - </Button>
+      <Button variant="outline-dark" > {counter} </Button>
+      <Button variant="outline-success" onClick={sumar}> + </Button>
+    </ButtonGroup>
+    </ButtonToolbar>
+    <ButtonToolbar
+        className="justify-content-between"
+        aria-label="Toolbar with Button groups"
+      >
+        <Button variant="outline-primary" > Agregar al carrito </Button>
+            </ButtonToolbar>
+
+    </>
+    // llamarlas funciones
+)
+}
+
+export default ItemCount
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import CartWidget from './CartWidget';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
